423711fcae17e9e80eba520346d0a28b279980c9
NIVETHA C <a1nivetha68@gmail.com>
2026-01-04 00:26:00 +0530

    Create encrypt_once.py
    This script uses the Fernet module from the cryptography library to securely generate an encryption key and encrypt a password. 
    It saves the key to 'secret.key' and prints the encrypted password to the console.

---
+++ encrypt_once.py
@@ -0,0 +1,20 @@
+from cryptography.fernet import Fernet
+
+def generate_key():
+    key = Fernet.generate_key()
+    with open("secret.key", "wb") as f:
+        f.write(key)
+    print("Key saved to 'secret.key'")
+
+def encrypt_password(password):
+    key = open("secret.key", "rb").read()
+    f = Fernet(key)
+    encrypted = f.encrypt(password.encode())
+    print("Encrypted password:")
+    print(encrypted)
+
+if __name__ == "__main__":
+
+    generate_key()
+
+    encrypt_password("Enter your DB password here")
